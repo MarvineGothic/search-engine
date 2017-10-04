9c5d4f580af4f3a3fe1f89a1d02a86bff94cb5d2
Rasmus Ravn Frost <rasf@itu.dk>
2017-10-04 15:59:31 +0200

    Renamed the old SearchEngine and created a new one that implements the Index interface

--- src/SearchEngine.java
+++ src/SearchEngine.java
@@ -12,38 +12,42 @@ public class SearchEngine {
         }
         Scanner sc = new Scanner(System.in);
 
-        // so we don't need the directory path:
-        // Now you only need to specify the relative path from the project folder to the datafile
-        String dir = System.getProperty("user.dir");
-        List<Website> sites = FileHelper.parseFile(dir + File.separator + "data" + File.separator + args[0]);
-        // ----------------------------------------------------------------
-        
+        List<Website> sites = FileHelper.loadFile(args[0]);
+
         System.out.println("These are some of the available sites");
-        for (int i = 0; i < Math.min(10, sites.size()); i++) {
+        for (int i = 0; i < Math.min(3, sites.size()); i++) {
             System.out.println(sites.get(i));
         }
 
-        System.out.println("Please provide a query word");
+        Index index;
+        System.out.println("Please select a SearchEngine type by typing a number:");
+        System.out.println("1 = ReverseIndex");
+        System.out.println("Otherwise: SimpleIndex");
+        String inputNumber = sc.nextLine();
+        if (inputNumber.equals("1"))
+            index = new ReverseIndex();
+        else
+            index = new SimpleIndex();
+        index.build(sites);
 
-        /**
-         * @author Sergiy
-         */
+        System.out.println("Please provide a query word");
         while (sc.hasNext()) {
             long startTime = System.nanoTime();
-            boolean contains = false;
+
             String line = sc.nextLine();
-            // Go through all websites and check if word is present
-            for (Website w : sites) {
-                if (w.containsWord(line)) {
-                    contains = true;
-                    System.out.println("Query is found on '" + w.getUrl() + "'");
-                    System.out.println("Response time: " + (System.nanoTime() - startTime) + " ns");
-                }
+            if (!index.validateQuery(line)){
+                System.out.println("Query is invalid");
+                continue;
             }
-            if (!contains) {
-                System.out.println("No website contains the query word.");
-                System.out.println("Response time: " + (System.nanoTime() - startTime) + " ns");
+            List<Website> foundSites = index.lookup(line);
+            for (Website w : foundSites) {
+                System.out.println("Query is found on '" + w.getUrl() + "'");
             }
+
+            if (foundSites.size() == 0) {
+                System.out.println("No websites matches the query");
+            }
+            System.out.println("Response time: " + (System.nanoTime() - startTime) + " ns");
             System.out.println("Please provide the next query word");
         }
     }
