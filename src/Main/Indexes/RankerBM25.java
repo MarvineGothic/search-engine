2670dced64a7fd8d32c9136cff935148e52ef18a
Andreas Tønder <atoe@itu.dk>
2017-11-07 15:44:43 +0100

    Added ranker classes

---
+++ src/Main/Indexes/RankerBM25.java
@@ -0,0 +1,32 @@
+package Main.Indexes;
+
+import Main.Website;
+
+import java.util.Collections;
+import java.util.List;
+
+public class RankerBM25 extends RankerIDF {
+    private int totalAmountOfWords;
+
+    public RankerBM25(List<Website> websiteList) {
+        super(websiteList);
+        for (Website site : sites) {
+            this.totalAmountOfWords += site.getWords().size();
+        }
+    }
+
+    @Override
+    public long getScore(String word, Website website, Index index) {
+        return idf(word, index) * tf(word, website);
+    }
+
+    private long tfplus(String word, Website website) {
+        long tf = this.tf(word, website);
+        double k = 1.75;
+        double b = 0.75;
+        int dl = website.getWords().size();
+        int avdl = totalAmountOfWords / sites.size();
+
+        return (long) (tf * (k + 1) / (k * (1 - b + b * dl / avdl + tf)));
+    }
+}
---
+++ src/Main/Indexes/RankerIDF.java
@@ -0,0 +1,35 @@
+package Main.Indexes;
+
+import Main.Website;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class RankerIDF implements IRanker {
+    protected List<Website> sites = new ArrayList<>();
+
+    public RankerIDF(List<Website> websiteList) {
+        sites = websiteList;
+    }
+
+    @Override
+    public long getScore(String word, Website website, Index index) {
+        return idf(word, index) * tf(word, website);
+    }
+
+    protected long tf(String word, Website website) {
+        return Collections.frequency(website.getWords(), word);
+    }
+
+    protected long idf(String word, Index index) {
+        double d = sites.size();
+        double n = index.lookup(word).size();
+
+        return (long) (Math.log(d / n) / Math.log(2));
+    }
+
+
+}
+
+
