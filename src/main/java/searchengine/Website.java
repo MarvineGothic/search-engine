8d378666561f6dafcf5eb17c7d965f3084b4331c
rale <rale@itu.dk>
2017-12-05 12:36:42 +0100

    no message

--- src/main/java/searchengine/Website.java
+++ src/main/java/searchengine/Website.java
@@ -4,23 +4,32 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 
+/**
+ * The Website class is the base element of the Search Engine, and is used for storing information in the database
+ * as well as searching the database.
+ */
 public class Website implements Comparable<Website> {
     private String title;
     private String url;
     private List<String> words;
     private HashSet<String> setOfWords;
 
+    /**
+     * Upon construction of a new Website element, the modifyWordlist method of IndexMethods is called on the list
+     * of keywords given to the constructor. This removes unnecessary punctuation and spaces, and converts the keywords
+     * to lower case, to remove case sensitivity in searches.
+     *
+     * To allow finding keywords in a Website without having to iterate through elements of a List, the words are
+     * deposited into a HashSet.
+     * @param url gives the location of the Website element
+     * @param title is the name of the Website element
+     * @param words is a list of the most commonly used keywords on a given website
+     */
     public Website(String url, String title, List<String> words) {
         this.url = url;
         this.title = title;
-
-        // process word the same way that queries are processed.
-        List<List<String>> wrappedWords = new ArrayList<>();
-        wrappedWords.add(words);
-        wrappedWords = IndexMethods.modifyQuery(wrappedWords);
-        words = wrappedWords.get(0);
-        this.words = words;
-        setOfWords = new HashSet<>(words);
+        this.words = IndexMethods.modifyWordList(words);
+        setOfWords = new HashSet<>(this.words);
     }
 
     public List<String> getWords() {
@@ -35,6 +44,11 @@ public class Website implements Comparable<Website> {
         return url;
     }
 
+    /**
+     *
+     * @param word
+     * @return
+     */
     public Boolean containsWord(String word) {
         return setOfWords.contains(word);
     }
@@ -43,9 +57,6 @@ public class Website implements Comparable<Website> {
         return setOfWords.containsAll(words);
     }
 
-    /**
-     * Author Rasmus F
-     */
     @Override
     public String toString() {
         String output = "Title: " + title + "\n";
@@ -55,8 +66,7 @@ public class Website implements Comparable<Website> {
     }
 
     /**
-     * Author: Rasmus F
-     * Get a list of what where the word occurs on the website.
+     * Get a list of where the word occurs on the website.
      *
      * @param word The word to check for.
      * @return A list of positions, where the positions is defined as the number of other words that occurs on the
@@ -72,8 +82,13 @@ public class Website implements Comparable<Website> {
         return wordPositions;
     }
 
+    /**
+     * The method compares a given Website with another by comparing the websites' URL.
+     * @param website a Website element to be compared with this Website
+     * @return returns true if the URLs are the same and false if they are not
+     */
     @Override
-    public int compareTo(Website o) {
-        return url.compareTo(o.url);
+    public int compareTo(Website website) {
+        return url.compareTo(website.url);
     }
 }
